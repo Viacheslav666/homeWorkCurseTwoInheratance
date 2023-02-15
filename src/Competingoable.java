b978b6aabcb9782e59619c3eb2bd335084f2f480
Viacheslav666 <cheep777@gmail.com>
2023-02-15 23:21:43 +0400

    Имплеминтировал enum

---
+++ src/Competingoable.java
@@ -0,0 +1,7 @@
+public interface Competingoable {
+    public abstract void pitStop();
+
+    public abstract void bestLapTimes(String time);
+
+    public abstract void maxSpeed();
+}
--- src/FreightCar.java
+++ src/FreightCar.java
@@ -1,13 +1,62 @@
 import Drivers.DriverD;
 
-public class FreightCar extends Transport <DriverD> {
-    public FreightCar(String brand, String model, int engineCapacity, DriverD driver) {
+import java.io.Serializable;
+
+public class FreightCar extends Transport<DriverD> {
+    public FreightCar(String brand, String model, int engineCapacity, DriverD driver, CarryingCapacity carryingCapacity) {
         super(brand, model, engineCapacity, driver);
+    }
+
+    public enum CarryingCapacity {
+        n1(0, 3.5f),
+        n2(3.5f, 12f),
+        n3(12, 0);
+        private float weightMax;
+        private float weightMin;
+
+        CarryingCapacity(float weightMin, float weightMax) {
+            this.weightMin = weightMin;
+            this.weightMax = weightMax;
+        }
+
+
+        CarryingCapacity() {
+        }
+
+        public float getWeightMax() {
+            return weightMax;
+        }
+
+        public void setWeightMax(float weightMax) {
+            this.weightMax = weightMax;
+        }
+
+        public float getWeightMin() {
+            return weightMin;
+        }
+
+        public void setWeightMin(float weightMin) {
+            this.weightMin = weightMin;
+        }
+
+        @Override
+        public String toString() {
+            return "Грузоподьемность{" +
+                    "Тип = " + this.name() + "\n"
+                    + (weightMin > 0 ? weightMin : "нижняя граница тон") +
+                    (weightMax > 0 ? weightMax : "верхняя граница тон") +
+                    '}';
+        }
     }
 
     @Override
     public String getBrand() {
         return brand;
+    }
+
+    @Override
+    void printType(Type name) {
+
     }
 
     @Override
@@ -38,5 +87,17 @@ public class FreightCar extends Transport <DriverD> {
         int maxSpeed = (int) (minBound + (maxBound - minBound) * Math.random());
         System.out.println("Максимальная скорость = " + maxSpeed);
 
+    }
+
+    @Override
+    Type getType() {
+        return Type.FREIGHTCAR;
+    }
+
+    @Override
+    void printType() {
+        String message = "Данных по транспортному средству недостаточно";
+        Serializable serializable = getType() == null ? message : getType();
+        System.out.println(serializable);
     }
 }
